329b4be27d4edaafa6800aef27b228ad7035716e
zpsaras <zach.psaras@gmail.com>
2015-12-03 19:04:02 -0500

    Most is working so far. Needs a timer and some layout tweaks.

--- Assets/Scripts/PresentAnimControl.cs
+++ Assets/Scripts/PresentAnimControl.cs
@@ -1,21 +1,44 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PresentAnimControl : MonoBehaviour {
+
+	public PresentationController controller;
+	public float timerStartVal = 20f;
+	private float timer;
 
 	public Animator leftPresent;
 	public Animator centerPresent;
 	public Animator rightPresent;
 
+	public GameObject TimerBarPanel;
+	public InputField answerField;
+
 	// State and transition names
-	const string k_TransitionName = "Done";
+	const string k_TransitionName	= "Done";
 	const string k_OutStateName		= "Out";
 
 	private int m_InParameterId;
+	private bool taskFinished = false;
+
+
+	void Start() {
+		timer = timerStartVal;
+		presentNew();
+	}
+
+
 
 	public void OnEnable() {
 		// Required to feed Animator.SetBool
 		m_InParameterId = Animator.StringToHash(k_TransitionName);
+	}
+
+	public void submit() {
+		grabAnswer();
+		controller.recordToArray(0.0f);
+		presentNew();
 	}
 
 	public void presentNew() {
@@ -23,6 +46,11 @@ public class PresentAnimControl : MonoBehaviour {
 		centerPresent.SetBool(m_InParameterId, true);
 		rightPresent.SetBool(m_InParameterId, true);
 		StartCoroutine(waitUntilAnimFinished());
+		setPresentationText();
+	}
+
+	public void grabAnswer() {
+		controller.setCurrentAnswer(answerField.text);
 	}
 
 	IEnumerator waitUntilAnimFinished() {
@@ -41,5 +69,27 @@ public class PresentAnimControl : MonoBehaviour {
 		leftPresent.SetBool(m_InParameterId, false);
 		centerPresent.SetBool(m_InParameterId, false);
 		rightPresent.SetBool(m_InParameterId, false);
+	}
+
+	void setPresentationText() {
+		char[] delims = { '/' };
+		string incLine = controller.getNewWords();
+		Text w1 = leftPresent.GetComponentInChildren<Text>();
+		Text w2 = centerPresent.GetComponentInChildren<Text>();
+		Text w3 = rightPresent.GetComponentInChildren<Text>();
+		Debug.Log("Received: " + incLine);
+		if (incLine.Equals(@"ERROR")) {
+			//End task.
+			w1.text = w2.text = w3.text = "DONE"; // TODO: CHANGE
+			taskFinished = true;
+			controller.logRecordedEntries();
+
+		} else {
+			string[] sep = incLine.Split(delims);
+			Debug.Log("W1: " + sep[0] + " / W2: " + sep[1] + " / W3: " + sep[2]);
+			w1.text = sep[0];
+			w2.text = sep[1];
+			w3.text = sep[2];
+		}
 	}
 }
---
+++ Assets/Scripts/PresentationController.cs
@@ -0,0 +1,170 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PresentationController : MonoBehaviour {
+
+	public int numberOfPresentations; // Because uint doesn't show up in the inspector
+
+	public TextAsset VeryEasyText;
+	public TextAsset EasyText;
+	public TextAsset MediumText;
+	public TextAsset HardText;
+
+	private int veryEasyCount,
+		easyCount,
+		mediumCount,
+		hardCount;
+
+	private string[] veryEasyLines;
+	private string[] easyLines;
+	private string[] mediumLines;
+	private string[] hardLines;
+
+	private string[][][] dataArray;
+
+	private int currDiff;
+	private int currLineNum;
+	private string currLine;
+	private string currAnswer;
+
+	// Use this for initialization
+	// Awake is used because this _must_ be initialized before PresentAnimControl.
+	void Awake () {
+		int i,j; // Used for array init
+
+		// Read in files, line by line.
+		// We will deal with shuffling in a second and parsing each line at presentation time
+		readText();
+		// Shuffle the lines
+			// TODO: Find a better way to use System.Random() rather than instatiating a new object every time
+		System.Random generator = new System.Random();
+		FisherYates(generator, veryEasyLines);
+		FisherYates(generator, easyLines);
+		FisherYates(generator, mediumLines);
+		FisherYates(generator, hardLines);
+
+		// Initialize Counts
+		veryEasyCount = easyCount = mediumCount = hardCount = numberOfPresentations;
+
+		// Initialize Data Array
+		dataArray = new string[4][][];
+		for (i = 0; i < 4; i++) {
+			dataArray[i] = new string[numberOfPresentations][];
+			for (j = 0; j < numberOfPresentations; j++) {
+				dataArray[i][j] = new string[3];
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void readText() {
+		veryEasyLines	= VeryEasyText.text.Split(new string[] { System.Environment.NewLine }, System.StringSplitOptions.None);
+		easyLines		= EasyText.text.Split(new string[] { System.Environment.NewLine }, System.StringSplitOptions.None);
+		mediumLines		= MediumText.text.Split(new string[] { System.Environment.NewLine }, System.StringSplitOptions.None);
+		hardLines		= HardText.text.Split(new string[] { System.Environment.NewLine }, System.StringSplitOptions.None);
+	}
+
+	public void setCurrentDifficulty(int diff) {
+		this.currDiff = diff;
+	}
+
+	public int getCurrentDifficulty() {
+		return this.currDiff;
+	}
+
+	public void setCurrentLine(string line) {
+		this.currLine = line;
+	}
+
+	public string getCurrentLine() {
+		return this.currLine;
+	}
+
+	public void setCurrentAnswer(string ans) {
+		this.currAnswer = ans;
+	}
+
+	public string getCurrentAnswer() {
+		return this.currAnswer;
+	}
+
+	public void setCurrentLineNumber(int num) {
+		this.currLineNum = num;
+	}
+
+	public int getCurrentLineNumber() {
+		return this.currLineNum;
+	}
+
+	public string getNewWords() {
+		// Are there presentation left of Very Easy?
+		if (veryEasyCount > 0) {
+			// Display Very Easy
+			veryEasyCount--;
+			setCurrentDifficulty(0);
+			setCurrentLine(veryEasyLines[veryEasyCount]);
+			setCurrentLineNumber(numberOfPresentations - veryEasyCount - 1); // Hmm...
+			return veryEasyLines[veryEasyCount];
+		} else if (easyCount > 0) {
+			// Display Easy
+			easyCount--;
+			setCurrentDifficulty(1);
+			setCurrentLine(easyLines[easyCount]);
+			setCurrentLineNumber(numberOfPresentations - easyCount - 1);
+			return easyLines[easyCount];
+		} else if (mediumCount > 0) {
+			// Display Medium
+			mediumCount--;
+			setCurrentDifficulty(2);
+			setCurrentLine(mediumLines[mediumCount]);
+			setCurrentLineNumber(numberOfPresentations - mediumCount - 1);
+			return mediumLines[mediumCount];
+		} else if (hardCount > 0) {
+			// Display Hard
+			hardCount--;
+			setCurrentDifficulty(3);
+			setCurrentLine(hardLines[hardCount]);
+			setCurrentLineNumber(numberOfPresentations - hardCount - 1);
+			return hardLines[hardCount];
+		} else {
+			// Nothing to display. Task finished.
+			return @"ERROR";
+		}
+	}
+
+	public void recordToArray(float responseTime) {
+		dataArray[this.currDiff][this.currLineNum][0] = this.currLine;
+		dataArray[this.currDiff][this.currLineNum][1] = this.currAnswer;
+		dataArray[this.currDiff][this.currLineNum][2] = responseTime.ToString();
+		Debug.Log("Recording data in: [" + this.currDiff + "][" + this.currLineNum + "]");
+	}
+
+	/* https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle */
+	/* This is probably a bit more generic than necessary but it'll be nice to reuse someday */
+	/* Provided by user `Matt Howells` at Stack Overflow */
+	public static void FisherYates<T>(System.Random rng, T[] arr) {
+		Debug.Log("Shuffling an array...");
+		int n = arr.Length;
+		while (n > 1) {
+			int k = rng.Next(n--);
+			T temp = arr[n];
+			arr[n] = arr[k];
+			arr[k] = temp;
+		}
+	}
+
+	public void logRecordedEntries() {
+		int i, j, k;
+		for (i = 0; i < 4; i++) {
+			for (j = 0; j < numberOfPresentations; j++) {
+				Debug.Log("[" + i + "][" + j + "][0]:" + dataArray[i][j][0]);
+				Debug.Log("[" + i + "][" + j + "][1]:" + dataArray[i][j][1]);
+				Debug.Log("[" + i + "][" + j + "][2]:" + dataArray[i][j][2]);
+			}
+		}
+	}
+}
